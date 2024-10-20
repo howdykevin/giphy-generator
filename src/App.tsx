57ba2d44ed96c666ded988118c32667a557b71af
kevvie <k.drago57@gmail.com>
2024-10-20 21:56:07 +1000

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { DisplayGrid } from "./components/DisplayGrid";
 
 function App() {
   return (
-    <div className="container mx-auto px-2 bg-gray-900">
+    <div className="container mx-auto px-2 bg-gray-900 h-screen">
       <SearchBar />
       <DisplayGrid />
     </div>
--- src/components/SkeletonGrid.tsx
+++ src/components/SkeletonGrid.tsx
@@ -1,7 +1,7 @@
 const SkeletonGrid = (): JSX.Element => {
   return (
     <>
-      {Array(6).fill(null).map((u,i) => (
+      {Array(9).fill(null).map((_,i) => (
         <div className="relative bg-slate-700 flex min-h-80 animate-pulse" key={i}/>
       ))}
     </>
